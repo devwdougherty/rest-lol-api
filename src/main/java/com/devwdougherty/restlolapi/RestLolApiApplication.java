f6f0057285dadc1d2d08fa5d3d6db3eddecd64d9
devwdougherty <devwdougherty@outlook.com>
2021-03-04 18:16:18 -0300

    initial code project

---
+++ src/main/java/com/devwdougherty/restlolapi/RestLolApiApplication.java
@@ -0,0 +1,13 @@
+package com.devwdougherty.restlolapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestLolApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestLolApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devwdougherty/restlolapi/RestLolApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devwdougherty.restlolapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestLolApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
